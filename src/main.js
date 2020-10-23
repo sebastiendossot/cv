8e7a173925d3c64734ef53fac3c249e4e9d685f3
sebastiendossot <sebastien.dossot@gmail.com>
2020-10-23 19:12:46 +0200

    another try

--- src/main.js
+++ src/main.js
@@ -8,17 +8,17 @@
 import './gtm.js';
 import Vue from 'vue';
 import App from './App';
-import { createGtm } from 'vue-gtm';
+import VueGtm from 'vue-gtm';
 import router from './router';
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-Vue.use(createGtm({
+Vue.use(VueGtm, {
   id: 'GTM-MTFSSFS' , // Your GTM single container ID or array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy']
   debug: true, // Whether or not display console logs debugs (optional)
   vueRouter: router, // Pass the router instance to automatically sync with router (optional)
-}));
+});
 
 /* eslint-disable no-new */
 new Vue({
